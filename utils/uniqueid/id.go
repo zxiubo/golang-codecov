f6ebc52f3917ebd04096281ccfcc8cf7377a0508
zhangxiubo <zxbdwy803@163.com>
2023-09-14 15:26:36 +0800

    git init

---
+++ utils/uniqueid/id.go
@@ -0,0 +1,90 @@
+package uniqueid
+
+// https://blog.kowalczyk.info/article/JyRZ/generating-good-unique-ids-in-go.html
+// To run:
+// go run main.go
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/chilts/sid"
+	guuid "github.com/google/uuid"
+	"github.com/kjk/betterguid"
+	"github.com/lithammer/shortuuid"
+	"github.com/oklog/ulid"
+	"github.com/rs/xid"
+	suuid "github.com/satori/go.uuid"
+	"github.com/segmentio/ksuid"
+	"github.com/sony/sonyflake"
+)
+
+func genShortUUID() {
+	id := shortuuid.New()
+	fmt.Printf("github.com/lithammer/shortuuid: %s\n", id)
+}
+
+func genUUID() {
+	id := guuid.New()
+	fmt.Printf("github.com/google/uuid:         %s\n", id.String())
+}
+
+func genXid() {
+	id := xid.New()
+	fmt.Printf("github.com/rs/xid:              %s\n", id.String())
+}
+
+func genKsuid() {
+	id := ksuid.New()
+	fmt.Printf("github.com/segmentio/ksuid:     %s\n", id.String())
+}
+
+func genBetterGUID() {
+	id := betterguid.New()
+	fmt.Printf("github.com/kjk/betterguid:      %s\n", id)
+}
+
+func genUlid() {
+	t := time.Now().UTC()
+	entropy := rand.New(rand.NewSource(t.UnixNano()))
+	id := ulid.MustNew(ulid.Timestamp(t), entropy)
+	fmt.Printf("github.com/oklog/ulid:          %s\n", id.String())
+}
+
+func genSonyflake() {
+	flake := sonyflake.NewSonyflake(sonyflake.Settings{})
+	if flake == nil {
+		fmt.Printf("Couldn't generate sonyflake.NewSonyflake. Doesn't work on Go Playground due to fake time.\n")
+		return
+	}
+	id, err := flake.NextID()
+	if err != nil {
+		log.Fatalf("flake.NextID() failed with %s\n", err)
+	}
+	// Note: this is base16, could shorten by encoding as base62 string
+	fmt.Printf("github.com/sony/sonyflake:      %x\n", id)
+}
+
+func genSid() {
+	id := sid.Id()
+	fmt.Printf("github.com/chilts/sid:          %s\n", id)
+}
+
+func genUUIDv4() {
+	id := suuid.NewV4()
+	fmt.Printf("github.com/satori/go.uuid:      %s\n", id)
+}
+
+func uniqueid() {
+	genXid()
+	genKsuid()
+	genBetterGUID()
+	genUlid()
+	genSonyflake()
+	genSid()
+	genShortUUID()
+	genUUIDv4()
+	genUUID()
+}
---
+++ utils/uniqueid/id_test.go
@@ -0,0 +1,7 @@
+package uniqueid
+
+import "testing"
+
+func Test_uniqueid(t *testing.T) {
+	uniqueid()
+}
